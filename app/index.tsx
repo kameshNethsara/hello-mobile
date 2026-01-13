baee25bf21525450d99a77796386f4971d97f273
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-13 23:08:00 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind and prettier-plugin-tailwindcss to package.json
    - Updated react-native-reanimated and react-native-safe-area-context versions
    - Added tailwindcss to package.json
    - Created tailwind.config.js with initial configuration for Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center bg-gray-50"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
