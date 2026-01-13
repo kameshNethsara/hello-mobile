684de1faba1ce2150fbe032b4e9688eb623cbab9
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-14 00:35:58 +0530

    feat: add Firebase integration and update TypeScript configuration
    - Added Firebase SDK to package.json.
    - Created firebase.ts service file for Firebase initialization.
    - Updated tsconfig.json to include nativewind-env.d.ts for type definitions.

---
+++ service/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA_Yiq13J_eTOZbWKLHV8lDNAzrhJdjQPc",
+  authDomain: "hello-mobile-e1474.firebaseapp.com",
+  projectId: "hello-mobile-e1474",
+  storageBucket: "hello-mobile-e1474.firebasestorage.app",
+  messagingSenderId: "153696241362",
+  appId: "1:153696241362:web:628c431c44997f31528f0b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
