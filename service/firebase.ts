83f34c1c36037c6c534406a50a15207ef74c6481
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-14 14:44:56 +0530

    update firebase

--- service/firebase.ts
+++ service/firebase.ts
@@ -2,16 +2,28 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_Yiq13J_eTOZbWKLHV8lDNAzrhJdjQPc",
-  authDomain: "hello-mobile-e1474.firebaseapp.com",
-  projectId: "hello-mobile-e1474",
-  storageBucket: "hello-mobile-e1474.firebasestorage.app",
-  messagingSenderId: "153696241362",
-  appId: "1:153696241362:web:628c431c44997f31528f0b"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// for auth
+//only need before firebase 9
+// export const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(AsyncStorage),
+// });
+export const auth = initializeAuth(app);
+
+//for db
+export const db = getFirestore(app);
