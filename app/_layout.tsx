8d9a1d6bf35f24bb4464e8821382487e125e414b
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-14 22:11:28 +0530

    feat: refactor layout and index components for improved structure and loading state handling

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
+import React from "react";
+import { Slot } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { LoaderProvider } from "@/context/LoaderContext";
+import {AuthProvider } from "@/context/AuthContext";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+  return (
+    <LoaderProvider>
+      <AuthProvider>
+        <SafeAreaProvider>
+          <SafeAreaView
+            className="flex-1"
+          >
+            <Slot />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </AuthProvider>
+    </LoaderProvider>
+   
+  );
+};
+
+export default RootLayout;
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,24 @@
-import { Text, View } from "react-native";
+import { useAuth } from "@/hooks/useAuth";
+import { Redirect } from "expo-router";
+import React from "react";
+import { ActivityIndicator, View } from "react-native";
 import "../global.css";
 
-export default function Index() {
-  return (
-    <View
-      className="flex-1 justify-center items-center bg-gray-50"
-    >
-      <Text className="">Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+const Index = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-gray-50">
+        <ActivityIndicator size="large" color="#4ade80" />
+      </View>
+    );
+  }
+  if (user) {
+    return <Redirect href="/(tabs)" />;
+  } else {
+    return <Redirect href={"/login"} />;
+  }
+};
+
+export default Index;
