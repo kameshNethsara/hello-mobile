a8f5586f927a9ac712319175d785a22258e788c8
kameshNethsara <kamesh317nethsara@gmail.com>
2026-01-25 00:01:11 +0530

    feat: implement authentication state handling in RootLayout; redirect users based on login status

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,37 @@
-import React from "react";
-import { Slot } from "expo-router";
+import React, { useEffect } from "react";
+import { Slot, useRouter } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { LoaderProvider } from "@/context/LoaderContext";
-import {AuthProvider } from "@/context/AuthContext";
+import { AuthProvider } from "@/context/AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/services/firebase";
 
 const RootLayout = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        router.replace("/(auth)/login");
+      } else {
+        router.replace("/(tabs)");
+      }
+    });
+
+    return unsub;
+  }, []);
+
   return (
     <LoaderProvider>
       <AuthProvider>
         <SafeAreaProvider>
-          <SafeAreaView
-            className="flex-1"
-          >
+          <SafeAreaView style={{ flex: 1 }}>
             <Slot />
           </SafeAreaView>
         </SafeAreaProvider>
       </AuthProvider>
     </LoaderProvider>
-   
+    
   );
 };
 
